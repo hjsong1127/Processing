364bf33ca1fd1be90b200c68a4a3141e8fbc2b2a
송현종 <jskwack9999@gmail.com>
2020-09-12 17:37:49 +0900

    숙제

---
+++ sketch_200911a/sketch_200911a.js
@@ -0,0 +1,19 @@
+function setup() {
+createCanvas(500,500);
+fill(182,0,0);
+ellipse(100,100,100,100);
+fill(100,0,0);
+ellipse(200,100,100,100);
+fill(300,0,0);
+ellipse(200,200,100,100);
+fill(200,300,0);
+ellipse(100,200,100,100);
+fill(100,100,100);
+ellipse(200,300,100,100);
+fill(500,120,200);
+ellipse(100,300,100,100);
+
+}
+function draw() {
+
+}
